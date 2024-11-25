7d940f1f6be440c8cfbbdadfab246ec8ce7a42d0
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-25 20:23:31 +0100

    Add sample code

--- preview/Program.cs
+++ preview/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChatAIze.SemanticIndex;
+
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
+var database = new SemanticDatabase("app.db", apiKey);
+
+//await database.AddAsync("cat", ["animal"]);
+//await database.AddAsync("dog", ["animal"]);
+//await database.AddAsync("apple", ["food"]);
+//await database.AddAsync("banana", ["food"]);
+
+var results = await database.FindAsync("wolf", tags: ["animal"], count: 2);
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
