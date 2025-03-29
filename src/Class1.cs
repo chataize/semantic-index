729fe8a8b91b266fb0cd9fa946a91ad0334b843b
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-03-29 13:59:48 +0100

    Add SemanticDatabase class

--- src/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ChatAIze.SemanticIndex;
-
-public class Class1
-{
-
-}
---
+++ src/SemanticDatabase.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics.Tensors;
+using System.Text.Json;
+using ChatAIze.GenerativeCS.Clients;
+
+namespace ChatAIze.SemanticIndex;
+
+public sealed class SemanticDatabase<T>
+{
+    private readonly OpenAIClient _client = new();
+
+    private List<SemanticRecord<T>> _records = [];
+
+    public async Task AddAsync(T item)
+    {
+        var json = JsonSerializer.Serialize(item);
+        var embedding = await _client.GetEmbeddingAsync(json);
+        var record = new SemanticRecord<T>(item, embedding);
+
+        _records.Add(record);
+    }
+
+    public async Task<List<T>> SearchAsync(string query, int count = 10)
+    {
+        var embedding = await _client.GetEmbeddingAsync(query);
+        var results = new SortedList<float, T>();
+
+        foreach (var record in _records)
+        {
+            var similarity = TensorPrimitives.Dot(record.Embedding, embedding);
+
+            if (results.Count < count)
+            {
+                while (results.ContainsKey(similarity))
+                {
+                    similarity += 1e-6f;
+                }
+
+                results.Add(similarity, record.Item);
+            }
+            else if (similarity > results.Keys[0])
+            {
+                while (results.ContainsKey(similarity))
+                {
+                    similarity += 1e-6f;
+                }
+
+                results.RemoveAt(0);
+                results.Add(similarity, record.Item);
+            }
+        }
+
+        return [.. results.Values.Reverse()];
+    }
+
+    public void Remove(T item)
+    {
+        _records.RemoveAll(r => r.Item!.Equals(item));
+    }
+
+    public async Task LoadAsync(string filePath)
+    {
+        using var stream = File.OpenRead(filePath);
+        _records = await JsonSerializer.DeserializeAsync<List<SemanticRecord<T>>>(stream) ?? [];
+    }
+
+    public async Task SaveAsync(string filePath)
+    {
+        using var stream = File.Create(filePath);
+        await JsonSerializer.SerializeAsync(stream, _records);
+    }
+}
