b850e099fad7baebe850560d8067a2cde1246eff
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-03-29 14:40:11 +0100

    Add example usage

--- preview/Program.cs
+++ preview/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChatAIze.SemanticIndex;
+
+var database = new SemanticDatabase<string>();
+
+if (!File.Exists("test-database.json"))
+{
+    await database.AddAsync("cat");
+    await database.AddAsync("dog");
+    await database.AddAsync("fish");
+
+    await database.AddAsync("apple");
+    await database.AddAsync("banana");
+    await database.AddAsync("orange");
+
+    await database.SaveAsync("test-database.json");
+}
+else
+{
+    await database.LoadAsync("test-database.json");
+}
+
+var animals = await database.SearchAsync("animal", 3);
+foreach (var animal in animals)
+{
+    Console.WriteLine(animal);
+}
